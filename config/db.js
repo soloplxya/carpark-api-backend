1617383bda104b44391ccf8f4063488696599959
soloplxya <69782911+soloplxya@users.noreply.github.com>
2022-03-03 01:52:17 +0800

    feat: add files to relevant folders

---
+++ config/db.js
@@ -0,0 +1,20 @@
+import { connect } from "mongoose"; 
+
+const { MONGO_URI } = process.env; 
+
+export function connect() {
+    connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      })
+      .then(() => {
+        console.log("Successfully connected to database");
+      })
+      .catch((error) => {
+        console.log("database connection failed. exiting now...");
+        console.error(error);
+        process.exit(1);
+      });
+}
